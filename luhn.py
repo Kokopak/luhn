b6957f8cdba8299f2b9e7b5baf9e393081d1b630
philippe@micro24 <philippe@chataignon.com>
2012-12-16 16:50:09 +0100

    Grosse simplification

--- luhn.py
+++ luhn.py
@@ -2,53 +2,30 @@
 #-*- coding: utf-8-*-
 
 def luhn(nombre):
-    listeChiffres = [int(chiffres) for chiffres in list(str(nombre))]
-    listeChiffres.reverse()
-    
     listeLuhn = []
-    index = 0
-    for chiffres in listeChiffres:
-        if index% 2 == 0:
-            listeLuhn.insert(index, chiffres)
+    for index, chiffre in enumerate(reversed([int(x) for x in list(str(nombre))])) :
+        if index % 2 == 0:
+            listeLuhn.append(chiffre)
         else:
-            doubleChiffre = chiffres * 2
-            sommeDouble = 0
-            if len(str(doubleChiffre)) > 1:
-                a = list(str(doubleChiffre))
-                sommeDouble = sum([int(el) for el in a])
-                listeLuhn.insert(index, sommeDouble)
-            else:
-                listeLuhn.insert(index, doubleChiffre)
-        index += 1
+            d = 2 * chiffre
+            n1, n2 = d/10, d%10 
+            listeLuhn.append(n1+n2)
+    return sum(listeLuhn)
 
-    listeLuhn.reverse()
-    return "".join(str(el) for el in listeLuhn)
+def isValide(n):
+    return luhn(n) % 10 == 0
 
-def isValide(nombre):
-    lisNombre = [int(el) for el in list(str(nombre))]
-    sommeNombre = sum(lisNombre)
-    
-    return sommeNombre % 10 == 0
+def cle(n):
+    c = (10 - luhn(10*n) % 10) % 10
+    return 10*n + c
 
-def sommeNombre(nombre):
-    lisNombre = [int(el) for el in list(str(nombre))]
-    return sum(lisNombre)
-    
 
-nbr = 54321 
-nbrLuhn = int(luhn(nbr))
+n = 3101997
+print n
+n = cle(n)
+print n
+print isValide(n)
 
-valide = isValide(nbrLuhn)
-
-if not valide:
-    print "Nombre non valide ! Recherche d'un chiffre de contrôle."
-    nbrLuhn = int(luhn(nbr))
-    valide = isValide(nbrLuhn)
-    nbr = int(str(nbr) + "0")
-    somNombre =sommeNombre(nbr)
-    x = 10 -(somNombre % 10) 
-    print "Chiffre de contrôle trouvé : %d" % x
-    nbr = int(str(nbr).replace(str(nbr)[-1], str(x)))
-    print "Le nouveau nombre qui marche est :%d " % (nbr)
-else:
-    print "Nombre valide ! Pas besoin de chiffre de contrôle."
+n = 6541897
+print n
+print isValide(n)
